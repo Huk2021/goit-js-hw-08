a67d59fbd35b41955668bfe9cbf7440860a265f0
Viktoriia Huk <relax16011991@gmail.com>
2021-10-23 23:52:40 +0300

    Задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+
+
+const galleryImages = document.querySelector('.gallery');
+const cardsImage = createGalleryItemCardsImage(galleryItems);
+
+galleryImages.insertAdjacentHTML('beforeend', cardsImage);
+
+function createGalleryItemCardsImage(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+             <img class="gallery__image"
+             src="${preview}"
+             alt="${description}" />
+            </a>
+        `;
+        })
+        .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    
+ });
+
 
 console.log(galleryItems);
