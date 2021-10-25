b7693ad443b4ea7ede04278f353c604a44b0d3c9
Viktoriia Huk <relax16011991@gmail.com>
2021-10-25 16:45:29 +0300

    правки задание 2 +  задание 3 начато

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,11 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const currentTime = localStorage.getItem("videoplayer-current-time");
+const LOCALSTORAGE_KEY = "videoplayer-current-time"
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
 
 player.setCurrentTime(currentTime);
 
-player.on('timeupdate', throttle(data => localStorage.setItem("videoplayer-current-time", data.seconds), 1000));
+player.on('timeupdate', throttle(data => localStorage.setItem(LOCALSTORAGE_KEY, data.seconds), 1000));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+
+// function onEmailInput(evt) {
+//   const email = evt.target.value;
+
+//     localStorage.setItem(LOCALSTORAGE_KEY, email);
+    
+// };
+
+// function onMessageInput(evt) {
+//    const message = evt.target.value;
+
+//     localStorage.setItem(LOCALSTORAGE_KEY, message);
+  
+// };
