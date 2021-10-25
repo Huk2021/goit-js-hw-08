ea4a3947e5167884ae94f962ac7991eba3cc0c06
Viktoriia Huk <relax16011991@gmail.com>
2021-10-25 14:14:40 +0300

    задание 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = localStorage.getItem("videoplayer-current-time");
+
+
+player.setCurrentTime(currentTime);
+
+player.on('timeupdate', throttle(data => localStorage.setItem("videoplayer-current-time", data.seconds), 1000));
 
